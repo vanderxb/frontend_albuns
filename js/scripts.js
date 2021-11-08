88da79a44d94c75f9551558cc31b8d2468154df1
Vander <vanderson.barbosa2@fatec.sp.gov.br>
2021-11-07 21:12:25 -0300

    frontend

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-// buscar o elemento no html da minha lista onde irei inserir as vagas
+
 const lista = document.getElementById('lista')
 
-// atribuindo a endpoint da api do backend em um constante
+
 const apiUrl = 'http://localhost:3004/albuns';
 
-// modo edicao e id edicao
+
 let edicao = false;
 let idEdicao = 0;
 
-// pegar os dados que o usuario digita no input (Elementos)
+
 let titulo = document.getElementById('titulo');
 let artista = document.getElementById('artista');
 let imagem = document.getElementById('imagem');
@@ -19,18 +19,16 @@ let nota = document.getElementById('nota');
 
 
 
-// faz uma resquisicao do tipo [GET] para o back que recebe todas as vagas cadastradas
+
 const getAlbuns = async () => {
-    // FETCH API api do javascript responsavel por fazer comunicacao entre requicoes http.
-    // faz uma requisicao [GET] para o backend na url http://localhost:3000/vagas
+    
     const response = await fetch(apiUrl)
-    // é a lista de objetos vagas (array de objetos)
+    
     const albuns = await response.json();
 
     console.log(albuns);
 
-    // a gente pega o resultado da api(um array de objetos com as vagas) e itera essa lista com o map
-    // algo parecido com um for.
+    
     albuns.map((album) => {
         if(album.status === false){
             lista.insertAdjacentHTML('beforeend', `
@@ -93,13 +91,13 @@ const getAlbuns = async () => {
 }
 
 
-// [POST] envia uma vaga para o backend para ser cadastrada
+
 
 const submitForm = async (event) => {
-    // previnir que o navegador atualiza a pagina por causa o evento de submit
+    
     event.preventDefault();
 
-    // Estamos construindo um objeto com os valores que estamos pegando no input.
+    
     const album = {
         titulo: titulo.value,
         artista: artista.value,
@@ -108,7 +106,7 @@ const submitForm = async (event) => {
         genero: genero.value
         
     }
-    // é o objeto preenchido com os valores digitados no input
+    
 
     if(edicao) {
         putAlbum(album, idEdicao);
@@ -121,7 +119,7 @@ const submitForm = async (event) => {
 }
 
 const createAlbum = async(album) => {
-    // estou construindo a requisicao para ser enviada para o backend.
+    
     const request = new Request(`${apiUrl}/add`, {
         method: 'POST',
         body: JSON.stringify(album),
@@ -130,19 +128,19 @@ const createAlbum = async(album) => {
         })
     })
 
-    // chamamos a funcao fetch de acordo com as nossa configuracaoes de requisicao.
+    
     const response = await fetch(request);
 
     const result = await response.json();
-    // pego o objeto que vem do backend e exibo a msg de sucesso em um alerta.
+    
     alert(result.message)
-    // vaga cadastrada com sucesso.
+    
     getAlbuns();
 
 }
 
 const putAlbum = async(album, id) => {
-    // estou construindo a requisicao para ser enviada para o backend.
+    
     const request = new Request(`${apiUrl}/edit/${id}`, {
         method:  'PUT',
         body: JSON.stringify(album),
@@ -151,11 +149,11 @@ const putAlbum = async(album, id) => {
         })
     })
 
-    // chamamos a funcao fetch de acordo com as nossa configuracaoes de requisicao.
+    
     const response = await fetch(request);
 
     const result = await response.json();
-    // pego o objeto que vem do backend e exibo a msg de sucesso em um alerta.
+    
     alert(result.message)
     edicao = false;
     idEdicao = 0;
@@ -163,9 +161,9 @@ const putAlbum = async(album, id) => {
 }
 
 
-// [DELETE] funcao que exclui um vaga de acordo com o seu id
+
 const deleteAlbum = async (id) => {
-    // construir a requiscao de delete
+    
     const request = new Request(`${apiUrl}/delete/${id}`, {
         method: 'DELETE'
     })
@@ -180,27 +178,23 @@ const deleteAlbum = async (id) => {
 }
 
 
-// [GET] /Vaga/{id} - funcao onde recebe um id via paramtero envia uma requisicao para o backend
-// e retorna a vaga de acordo com esse id.
+
 const getAlbumById = async (id) => {
     const response = await fetch(`${apiUrl}/${id}`);
     return await response.json();
 }
 
 
-// ao clicar no botao editar
-// ela vai preencher os campos dos inputs
-// para montar o objeto para ser editado
+
 const editAlbum = async (id) => {
-    // habilitando o modo de edicao e enviando o id para variavel global de edicao.
+    
     edicao = true;
     idEdicao = id;
 
-    //precismo buscar a informacao da vaga por id para popular os campos
-    // salva os dados da vaga que vamos editar na variavel vaga.
+  
     const album = await getAlbumById(id);
 
-    //preencher os campos de acordo com a vaga que vamos editar.
+    
     titulo.value = album.titulo;
     artista.value =  album.artista;
     imagem.value = album.imagem;
@@ -218,32 +212,32 @@ const clearFields = () => {
 }
 
 const setFalse = async(id) => {
-    // estou construindo a requisicao para ser enviada para o backend.
+   
     const status = false
     const request = new Request(`${apiUrl}/${status}/${id}`, {
         method:  'PUT'
     })
 
-    // chamamos a funcao fetch de acordo com as nossa configuracaoes de requisicao.
+   
     const response = await fetch(request);
     const result = await response.json();
-    // pego o objeto que vem do backend e exibo a msg de sucesso em um alerta.  
+      
 
     lista.innerHTML = '';
     getAlbuns();
 }
 
 const setTrue = async(id) => {
-    // estou construindo a requisicao para ser enviada para o backend.
+    
     const status = true
     const request = new Request(`${apiUrl}/${status}/${id}`, {
         method:  'PUT'
     })
 
-    // chamamos a funcao fetch de acordo com as nossa configuracaoes de requisicao.
+    
     const response = await fetch(request);
     const result = await response.json();
-    // pego o objeto que vem do backend e exibo a msg de sucesso em um alerta.  
+    
 
     lista.innerHTML = '';
     getAlbuns();
